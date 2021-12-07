45b0ee7fa6dd1cfd54e7143831d2feccbb5ec708
OlivierMETREAU <metreau.olivier@orange.fr>
2021-12-07 21:05:19 +0100

    day7 completed

--- src/day6.py
+++ src/day6.py
@@ -2,7 +2,7 @@ input_choices = [[3,4,3,1,2], [1,1,3,5,1,3,2,1,5,3,1,4,4,4,1,1,1,3,1,4,3,1,2,2,2
 
 def first_star(data, number_of_days):
    number_of_fish_per_day = [data.count(x) for x in range(9)]
-   for day in range(1, number_of_days + 1):
+   for _ in range(1, number_of_days + 1):
       number_of_babies = number_of_fish_per_day[0]
       number_of_fish_per_day = number_of_fish_per_day[1:] + [number_of_babies]
       number_of_fish_per_day[6] += number_of_babies
